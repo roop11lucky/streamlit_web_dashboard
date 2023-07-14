9c90f4e57b023be2c765de6cd2547abff5a87cee
Roopkumar <38459557+roop11lucky@users.noreply.github.com>
2023-07-14 13:27:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,174 +1,43 @@
-# import altair as alt
-# import pandas as pd
-# import streamlit as st
-# from vega_datasets import data
+import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-# st.set_page_config(
-#     page_title="Time series annotations", page_icon="⬇", layout="centered"
-# )
+# Sample data
+data = pd.DataFrame({
+    'Category': ['A', 'B', 'C', 'D'],
+    'Count': [10, 20, 15, 30]
+})
 
+# Create a bar chart function
+def create_bar_chart(data):
+    fig = px.bar(data, x='Category', y='Count')
+    return fig
 
-# @st.experimental_memo
-# def get_data():
-#     source = data.stocks()
-#     source = source[source.date.gt("2004-01-01")]
-#     return source
+# Create a pie chart function
+def create_pie_chart(data):
+    fig = px.pie(data, values='Count', names='Category')
+    return fig
 
+# Set the title of the dashboard
+st.title('Interactive Dashboard')
 
-# @st.experimental_memo(ttl=60 * 60 * 24)
-# def get_chart(data):
-#     hover = alt.selection_single(
-#         fields=["date"],
-#         nearest=True,
-#         on="mouseover",
-#         empty="none",
-#     )
+# Create the pie chart
+pie_chart = create_pie_chart(data)
+pie_clicked = st.plotly_chart(pie_chart)
 
-#     lines = (
-#         alt.Chart(data, height=500, title="Evolution of stock prices")
-#         .mark_line()
-#         .encode(
-#             x=alt.X("date", title="Date"),
-#             y=alt.Y("price", title="Price"),
-#             color="symbol",
-#         )
-#     )
+# Handle click events on the pie chart
+selected_category = None
+if pie_clicked:
+    clicked_data = pie_clicked.expand_data()
+    if clicked_data and 'points' in clicked_data[0]:
+        selected_category = clicked_data[0]['points'][0]['label']
 
-#     # Draw points on the line, and highlight based on selection
-#     points = lines.transform_filter(hover).mark_circle(size=65)
+# Apply filters to the data based on the selected category
+if selected_category:
+    filtered_data = data[data['Category'] == selected_category]
+else:
+    filtered_data = data
 
-#     # Draw a rule at the location of the selection
-#     tooltips = (
-#         alt.Chart(data)
-#         .mark_rule()
-#         .encode(
-#             x="yearmonthdate(date)",
-#             y="price",
-#             opacity=alt.condition(hover, alt.value(0.3), alt.value(0)),
-#             tooltip=[
-#                 alt.Tooltip("date", title="Date"),
-#                 alt.Tooltip("price", title="Price (USD)"),
-#             ],
-#         )
-#         .add_selection(hover)
-#     )
-
-#     return (lines + points + tooltips).interactive()
-
-
-# st.title("⬇ Time series annotations")
-
-# st.write("Give more context to your time series using annotations!")
-
-# col1, col2, col3 = st.columns(3)
-# with col1:
-#     ticker = st.text_input("Choose a ticker (⬇💬👇ℹ️ ...)", value="⬇")
-# with col2:
-#     ticker_dx = st.slider(
-#         "Horizontal offset", min_value=-30, max_value=30, step=1, value=0
-#     )
-# with col3:
-#     ticker_dy = st.slider(
-#         "Vertical offset", min_value=-30, max_value=30, step=1, value=-10
-#     )
-
-# # Original time series chart. Omitted `get_chart` for clarity
-# source = get_data()
-# chart = get_chart(source)
-
-# # Input annotations
-# ANNOTATIONS = [
-#     ("Mar 01, 2008", "Pretty good day for GOOG"),
-#     ("Dec 01, 2007", "Something's going wrong for GOOG & AAPL"),
-#     ("Nov 01, 2008", "Market starts again thanks to..."),
-#     ("Dec 01, 2009", "Small crash for GOOG after..."),
-# ]
-
-# # Create a chart with annotations
-# annotations_df = pd.DataFrame(ANNOTATIONS, columns=["date", "event"])
-# annotations_df.date = pd.to_datetime(annotations_df.date)
-# annotations_df["y"] = 0
-# annotation_layer = (
-#     alt.Chart(annotations_df)
-#     .mark_text(size=15, text=ticker, dx=ticker_dx, dy=ticker_dy, align="center")
-#     .encode(
-#         x="date:T",
-#         y=alt.Y("y:Q"),
-#         tooltip=["event"],
-#     )
-#     .interactive()
-# )
-
-# # Display both charts together
-# st.altair_chart((chart + annotation_layer).interactive(), use_container_width=True)
-
-# st.write("## Code")
-
-# st.write(
-#     "See more in our public [GitHub"
-#     " repository](https://github.com/streamlit/example-app-time-series-annotation)"
-# )
-
-# st.code(
-#     f"""
-# import altair as alt
-# import pandas as pd
-# import streamlit as st
-# from vega_datasets import data
-
-# @st.experimental_memo
-# def get_data():
-#     source = data.stocks()
-#     source = source[source.date.gt("2004-01-01")]
-#     return source
-
-# source = get_data()
-
-# # Original time series chart. Omitted `get_chart` for clarity
-# chart = get_chart(source)
-
-# # Input annotations
-# ANNOTATIONS = [
-#     ("Mar 01, 2008", "Pretty good day for GOOG"),
-#     ("Dec 01, 2007", "Something's going wrong for GOOG & AAPL"),
-#     ("Nov 01, 2008", "Market starts again thanks to..."),
-#     ("Dec 01, 2009", "Small crash for GOOG after..."),
-# ]
-
-# # Create a chart with annotations
-# annotations_df = pd.DataFrame(ANNOTATIONS, columns=["date", "event"])
-# annotations_df.date = pd.to_datetime(annotations_df.date)
-# annotations_df["y"] = 0
-# annotation_layer = (
-#     alt.Chart(annotations_df)
-#     .mark_text(size=15, text="{ticker}", dx={ticker_dx}, dy={ticker_dy}, align="center")
-#     .encode(
-#         x="date:T",
-#         y=alt.Y("y:Q"),
-#         tooltip=["event"],
-#     )
-#     .interactive()
-# )
-
-# # Display both charts together
-# st.altair_chart((chart + annotation_layer).interactive(), use_container_width=True)
-
-# """,
-#     "python",
-# )
-
-# success
-st.success("Success")
-
-# success
-st.info("Information")
-
-# success
-st.warning("Warning")
-
-# success
-st.error("Error")
-
-# Exception - This has been added later
-exp = ZeroDivisionError("Trying to divide by Zero")
-st.exception(exp)
+# Create the bar chart with the filtered data
+bar_chart = create_bar_chart(filtered_data)
+st.plotly_chart(bar_chart)
